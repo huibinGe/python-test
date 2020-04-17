9424101c5fcf5e99e6065d7001167222380c7a05
葛慧斌 <gehuibin@gehuibindeMacBook-Air.local>
2020-04-17 22:32:32 +0800

    [ADD新增查询页面

--- views/bussiness.py
+++ views/bussiness.py
@@ -18,10 +18,7 @@ def delete_commodities(id):
     commodity = Commodity.query.all()
     return render_template("bussiness/commoditylist.html", commodity=commodity)
 
-@buss_page.route('/commoditylist/query')
-def query():
-    commodity = Commodity.query.filter(Commodity.id==id).first()
-    return render_template('bussiness/hello.html', commodity=commodity)
+
 
 @buss_page.route('/commoditylist/edit', methods=['GET', 'PoSt'])
 def edit():
@@ -35,7 +32,7 @@ def edit():
         db.session.commit()
         commodity = Commodity.query.all()
         return render_template('bussiness/editcommo_success.html')
-@buss_page.route('/commoditylist/add', methods=['GET', 'PoSt'])
+@buss_page.route('/commoditylist/add', methods=['GET', 'POST'])
 def add():
         name = request.form.get('name')
         price = request.form.get('price')
@@ -45,7 +42,12 @@ def add():
         commodity = Commodity.query.all()
         return render_template('bussiness/commoditylist.html', commodity=commodity)
 
-
+@buss_page.route('/commoditylist/query',methods=['GET', 'POST'])
+def query():
+    name = request.form.get('name')
+    commodity = Commodity.query.filter(Commodity.name==name).first()
+    print(111111)
+    return render_template('bussiness/commodity_query.html', commodity=commodity)
 
 
 
