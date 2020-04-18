559af997ff1cb5fe482a0f0619fa2b08a00fdec4
葛慧斌 <gehuibin@gehuibindeMacBook-Air.local>
2020-04-18 11:21:32 +0800

    [ADD]商家客户

--- models.py
+++ models.py
@@ -32,36 +32,43 @@ class Orders(db.Model):
     __tablename__ = 'Orders'
     #物流
     id = db.Column(db.Integer, primary_key=True)
-    o_name = db.Column(db.String(80), unique=True, nullable=True, default='')
+    o_name = db.Column(db.String(80), unique=False, nullable=True, default='')
     o_time = db.Column(db.DateTime, nullable=True)
     location = db.Column(db.String(80), unique=False, nullable=True, default='')
     person = db.Column(db.String(20), unique=False, nullable=True)
     tel = db.Column(db.String(80), unique=False, nullable=True)
     desc = db.Column(db.String(200), unique=False, nullable=True)
+    comp = db.Column(db.String(100), unique=False, nullable=True)
     status = db.Column(db.String(5), unique=False, nullable=True)
 
-    def __init__(self, o_name, o_time, location, person, tel, desc, status):
+    def __init__(self, o_name, o_time, location, person, tel, desc,comp, status):
         self.o_name = o_name
         self.o_time = o_time
         self.location = location
         self.person = person
         self.tel = tel
         self.desc = desc
+        self.comp = comp
         self.status = status
 
 
 def create_data():
     db.drop_all()
     db.create_all()
-    user1 = User("张三", "111@qq.com", "111111", "商家")
-    user2 = User("李四", "222@qq.com", "222222", "用户")
+    user1 = User("张三", "111@qq.com", "111111", "厂商")
+    user2 = User("李四", "222@qq.com", "222222", "商家")
     user3 = User("王五", "333@qq.com", "333333", "物流公司")
     c1 = Commodity("王老吉", 5)
     c2 = Commodity("可口可乐", 3)
     c3 = Commodity("旺旺雪饼", 4)
-    o1 = Orders("王老吉",'2019-03-13 11:35:52.13',"杭州","李四","13843558644","西安","已出库")
-    o2 = Orders("可口可乐",'2019-04-05 06:33:35.33',"上海","李四","13843558644","贵阳","已发货")
-    o3 = Orders("旺旺雪饼",'2019-03-05 01:53:55.63',"福州","李四","13843558644","西安","已出库")
+    o1 = Orders("王老吉",'2019-03-13 11:35:52.13',"杭州","李四","13843558644","西安","顺丰","已出库")
+    o2 = Orders("可口可乐",'2019-04-05 06:33:35.33',"上海","李四","13843558644","贵阳","顺丰","已发货")
+    o3 = Orders("旺旺雪饼",'2019-03-05 01:53:55.63',"福州","李四","13843558644","西安","申通","已出库")
+    o4 = Orders("旺旺雪饼",'2019-04-05 01:53:55.63',"福州","李四","13843558644","西安","顺丰","已签收")
+    o5 = Orders("旺旺雪饼",'2019-04-05 01:53:55.63',"福州","李四","13843558644","西安","ems","已出厂")
+    o6 = Orders("旺旺雪饼",'2019-04-05 01:53:55.63',"福州","李四","13843558644","西安","顺丰","已发货")
+
+
     db.session.add(user1)
     db.session.add(user2)
     db.session.add(user3)
@@ -71,6 +78,9 @@ def create_data():
     db.session.add(o1)
     db.session.add(o2)
     db.session.add(o3)
+    db.session.add(o4)
+    db.session.add(o5)
+    db.session.add(o6)
     db.session.commit()
 
 
