5f1662c5048c826110ba98a210ba17ff23a1b053
da1234cao <17355051286@163.com>
2020-04-23 11:36:57 +0800

    for_pass

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ from .views.warehouse import  ware_page
 from .views.admin import admin_page
 from .extension import db
 app = Flask('python-test')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Py1438222@localhost:3306/python_sql'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://dacao:1735505@localhost:3306/python_test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "lalskskskskksksjsj"
 db.init_app(app)
