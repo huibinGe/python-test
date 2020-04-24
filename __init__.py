bcfe1e597aa800c7f329f8da652313857cacf8ba
da1234cao <17355051286@163.com>
2020-04-24 13:18:42 +0800

    new from master status

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ from .extension import db
 
 app = Flask('python-test')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:11111111@localhost:3306/user2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://dacao:1735505@localhost:3306/python_test'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "lalskskskskksksjsj"
